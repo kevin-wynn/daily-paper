8785f94b5d2003d8ba74500d3dbde1663330b6de
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2023-08-06 15:19:41 -0500

    add cf adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), preact({ compat: true })],
+  integrations: [tailwind(), preact({
+    compat: true
+  })],
+  adapter: cloudflare()
 });
