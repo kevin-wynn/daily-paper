2deeb309e4a4be68527925ec3d5f74af7fbfa81a
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2023-08-06 23:10:09 -0500

    adjust some sources

---
+++ src/components/NewsItem.tsx
@@ -0,0 +1,55 @@
+import { useStore } from "@nanostores/preact";
+import type { NewsItemType } from "../pages/index.astro";
+import { NewsSourceTitle } from "./NewsSourceTitle";
+import { collapsedItems } from "../stores/collapsedItemsStore";
+
+export const NewsItem = ({
+  newsItem,
+  lastBuildDate,
+  items,
+}: {
+  newsItem: NewsItemType;
+  lastBuildDate: string;
+  items: any;
+}) => {
+  const $collapsedItems = useStore(collapsedItems);
+  const htmlFriendlyText = newsItem.name.toLowerCase().replaceAll(" ", "_");
+
+  return (
+    <>
+      <NewsSourceTitle
+        name={newsItem.name}
+        sourceLogo={newsItem.sourceLogo}
+        sourceURL={newsItem.sourceURL}
+        lastBuildDate={lastBuildDate}
+      />
+      <div
+        class={`grid md:gap-6 grid-cols-1 md:grid-cols-3 ${
+          ($collapsedItems as string[]).includes(htmlFriendlyText)
+            ? "hidden"
+            : ""
+        }`}
+      >
+        {items.map((newsItem: any) => {
+          return (
+            <a
+              href={newsItem.link._text}
+              target="_blank"
+              class="p-4 mb-4 hover:bg-slate-200"
+            >
+              <h3 class="text-xl underline">
+                {newsItem.title?._text || newsItem.description?._cdata}
+              </h3>
+              <span class="font-thin no-underline">
+                {new Date(newsItem.pubDate._text).toLocaleString()}
+              </span>
+              <p class="mt-4">
+                {newsItem.description?._text || newsItem.description?._cdata}
+              </p>
+            </a>
+          );
+        })}
+      </div>
+    </>
+  );
+};
--- src/components/NewsSourceList.tsx
+++ src/components/NewsSourceList.tsx
@@ -1,22 +1,19 @@
 import { useStore } from "@nanostores/preact";
-import { collapsedItems } from "../stores/collapsedItemsStore";
-import { NewsSourceTitle } from "./NewsSourceTitle";
-import type { NewsItem } from "../pages/index.astro";
 import { activeNewsSource } from "../stores/activeNewsSourceStore";
 import type { FilterSourcesType } from "./NewsSourceFilter";
+import { NewsItem } from "./NewsItem";
+import type { NewsItemType } from "../pages/index.astro";
 
 export const NewsSourceList = ({
   newsItem,
   items,
   lastBuildDate,
 }: {
-  newsItem: NewsItem;
+  newsItem: NewsItemType;
   items: any;
   lastBuildDate: string;
 }) => {
-  const $collapsedItems = useStore(collapsedItems);
   const $activeNewsSource = useStore(activeNewsSource);
-  const htmlFriendlyText = newsItem.name.toLowerCase().replaceAll(" ", "_");
 
   const activeNewsSources = $activeNewsSource.filter(
     (source: FilterSourcesType) => {
@@ -35,35 +32,11 @@ export const NewsSourceList = ({
     <div class="mb-12">
       {isActiveNewsSource &&
         (isActiveNewsSource as FilterSourcesType).active && (
-          <>
-            <NewsSourceTitle
-              name={newsItem.name}
-              sourceLogo={newsItem.sourceLogo}
-              sourceURL={newsItem.sourceURL}
-              lastBuildDate={lastBuildDate}
-            />
-            <div
-              class={`grid md:gap-6 grid-cols-1 md:grid-cols-3 ${
-                ($collapsedItems as string[]).includes(htmlFriendlyText)
-                  ? "hidden"
-                  : ""
-              }`}
-            >
-              {items.map((newsItem: any) => (
-                <a
-                  href={newsItem.link._text}
-                  target="_blank"
-                  class="hover:bg-slate-200 p-4 mt-8"
-                >
-                  <h3 class="text-xl underline">{newsItem.title._text} </h3>
-                  <span class="font-thin no-underline">
-                    {new Date(newsItem.pubDate._text).toLocaleString()}
-                  </span>
-                  <p class="mt-4">{newsItem.description._text}</p>
-                </a>
-              ))}
-            </div>
-          </>
+          <NewsItem
+            items={items}
+            newsItem={newsItem}
+            lastBuildDate={lastBuildDate}
+          />
         )}
     </div>
   );
