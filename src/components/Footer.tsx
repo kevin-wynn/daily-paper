00f01c25717eedd3757e2a05eb600e46a0bde367
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2023-08-07 21:42:03 -0500

    add about page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <div class="flex">
+      <a class="text-blue-500 hover:text-blue-800 mr-4" href="/">
+        Home
+      </a>
+      <a class="text-blue-500 hover:text-blue-800" href="/about">
+        About
+      </a>
+    </div>
+  );
+};
