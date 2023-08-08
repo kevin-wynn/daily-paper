cb51a46a10d3d433ebcd121f0c764aad3f74b656
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2023-08-07 21:44:29 -0500

    break out header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div>
+      <h1 class="text-5xl md:text-9xl underline mb-2">
+        <a href="/">The Daily Paper</a>
+      </h1>
+      <p class="text-xl md:text-4xl mb-12">
+        Your daily source for aggregated news.
+      </p>
+    </div>
+  );
+};
